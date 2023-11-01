b6263b94b08162ef70981120904f631be031ee64
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-11-01 12:04:58 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/login_page/login_page_widget.dart
+++ lib/pages/login_page/login_page_widget.dart
@@ -366,12 +366,8 @@ class _LoginPageWidgetState extends State<LoginPageWidget> {
                                                 .ceoTokenResponse?.succeeded ??
                                             true)) {
                                           setState(() {
-                                            FFAppState().userId = getJsonField(
-                                              (_model.ceoTokenResponse
-                                                      ?.jsonBody ??
-                                                  ''),
-                                              r'''$.userId''',
-                                            ).toString();
+                                            FFAppState().userId = _model
+                                                .emailAddressController.text;
                                             FFAppState().token = getJsonField(
                                               (_model.ceoTokenResponse
                                                       ?.jsonBody ??
